10a47fbd560482cc7aa9501b6864f1234b31da12
khg1217 <gohangyeol54@gmail.com>
2024-02-21 10:32:31 +0900

    first

---
+++ src/main/java/org/ohgiraffers/board/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package org.ohgiraffers.board.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Board Mission💫",
+        description = "Board Mission Api 명세",
+        version = "v1")
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi firstOpenApi() {
+        String[] path = {
+                "org.ohgiraffers.board.controller"
+        };
+
+        return GroupedOpenApi.builder()
+                .group("1. 게시글 관리")
+                .packagesToScan(path)
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi secondOpenApi() {
+        String[] path = {
+                ""
+        };
+
+        return GroupedOpenApi.builder()
+                .group("2. 아직 미정")
+                .packagesToScan(path)
+                .build();
+    }
+}
